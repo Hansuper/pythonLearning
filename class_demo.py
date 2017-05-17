91b5a057fe815753417d676b7f94b120d751f368
hanxu <harrod7890@163.com>
2017-05-17 09:07:54 +0800

    tt

--- class_demo.py
+++ class_demo.py
@@ -8,7 +8,7 @@ __author__ = 'harrod'
 # 类最终都会继承的类  
 
 class Student(object):
-    # __init__第一个参数永远是self,表示创建的实例本身
+    # __init__第一个参数永远是self,表示创建 的实例本身
     # name,score这两个参数在实例化的时候  就必须传入了
     def __init__(self,name,score):
         self.name = name
